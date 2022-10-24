98688a34f0ef6796d9027810cede8f995d7204ff
Adnan <adnanmkhan2003@gmail.com>
2022-10-23 20:59:41 -0700

    All Files committed

---
+++ js/app.js
@@ -0,0 +1,19 @@
+let navlinks = document.getElementById('navLinks');
+
+function openMenu(){
+    navLinks.style.right = "0";
+}
+
+function closeMenu(){
+    navLinks.style.right = "-800px";
+}
+
+{/* <ul class="about-points">
+<p class="about-bold">Contractor Requirements:</p> </b> <br>
+<li>Bachelor’s degree in construction science, building science, surveying, or construction safety.</li>
+<li>At least 5 years experience in construction.</li>
+<li>Relevant license and bonds.</li>
+<li>Strong business and communication skills.</li>
+<li>Project Management courses would be beneficial.</li>
+<li>Experience using project management software.</li>
+</ul> */}
